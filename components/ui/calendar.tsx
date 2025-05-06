f4e8b92520e13b20861727899e0db4d7fca6af89
Whart0n <code@Matts-MacBook-Pro.local>
2025-05-05 18:28:39 -0600

    fix: remove custom icons in calendar component to resolve TypeScript errors

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -53,8 +53,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        // Removed custom navigation icons to use default ones
+        // This avoids TypeScript errors with react-day-picker component types
       }}
       {...props}
     />
@@ -63,42 +63,3 @@ function Calendar({
 Calendar.displayName = "Calendar"
 
 export { Calendar }
-
-// Simple implementation of Chevron icons
-function ChevronLeft(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <polyline points="15 18 9 12 15 6" />
-    </svg>
-  )
-}
-
-function ChevronRight(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <polyline points="9 18 15 12 9 6" />
-    </svg>
-  )
-}
