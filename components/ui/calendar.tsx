1123ecd6b84124ecde8b4281ee1fd7268edf2066
Whart0n <code@Matts-MacBook-Pro.local>
2025-05-22 17:05:43 -0600

    Fix calendar component to use default icons and resolve TypeScript errors

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -47,40 +47,6 @@ function Calendar({
           day_hidden: "invisible",
           ...classNames,
         }}
-        components={{
-          IconPrevious: () => (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="h-4 w-4"
-            >
-              <path d="m15 18-6-6 6-6" />
-            </svg>
-          ),
-          IconNext: () => (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="h-4 w-4"
-            >
-              <path d="m9 18 6-6-6-6" />
-            </svg>
-          ),
-        }}
         {...props}
       />
     </div>
